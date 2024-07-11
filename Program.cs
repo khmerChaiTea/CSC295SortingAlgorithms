283d66ebaf8ecac75807c58490f6face91df8dc7
pchai <prempreychhay@gmail.com>
2024-07-11 14:37:26 -0700

    update 2.37

--- Program.cs
+++ Program.cs
@@ -2,11 +2,43 @@
 
 namespace SortingAlgorithms
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] arr1 = { 90, 3, 2, 56, 32, 34, 65, 68, 76, 1, 0, 100, 8 };
+            PrintArray(arr1);
+            BubbleSort(arr1);
+            PrintArray(arr1);
+        }
+
+        public static void PrintArray(int[] arr)
+        {
+            foreach (int element in arr)
+            {
+                Console.Write($"{element} ");
+            }
+            Console.WriteLine();
+        }
+
+        public static Array BubbleSort(int[] arrToSort)
+        {
+            int temp;
+            for (int i = 0; i < arrToSort.Length - 1; i++)          // how many times it's going through unsorted array
+            {
+                for (int j = 0; j < arrToSort.Length - 1 - i; j++)
+                {
+                    // need to swap
+                    if (arrToSort[j] > arrToSort[j + 1])
+                    {
+                        temp = j;
+                        arrToSort[j] = arrToSort[j + 1];
+                        arrToSort[j + 1] = temp;
+                    }
+                }
+            }
+
+            return arrToSort;
         }
     }
 }
